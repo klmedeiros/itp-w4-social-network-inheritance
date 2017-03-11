5f04d53afdcf3d6cc0fc9d71fa4257c8c034cb41
louise <kaelenmedeiros@gmail.com>
2017-03-11 17:23:59 +0000

    inital commit social network

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,24 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        
+        self.posts = []
+        self.following = []
 
     def add_post(self, post):
-        pass
+        #how do we know what user it is?
+        post.set_user(self)
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        timeline_posts = []
+        for user in self.following:
+            timeline_posts += user.posts
+            
+        return timeline_posts
 
     def follow(self, other):
-        pass
+        self.following.append(other)
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,69 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        self.timestamp = timestamp or datetime.now()
+        self.user = None
+
+    def set_user(self, user):
+        self.user = user
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post): #Inherit properly
+    def __init__(self, text, timestamp=None):
+        super(TextPost, self).__init__(text, timestamp)
 
+    def __str__(self):
+        return '@{first_name} {last_name}: "{text}"\n\t{date}'.format(
+            first_name=self.user.first_name,
+            last_name=self.user.last_name,
+            text=self.text,
+            date=self.timestamp.strftime("%A, %b %d, %Y"))
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+"""
+John Lennon: "All you need is love!"
+  Friday, Feb 03, 2017
+"""
 
+class PicturePost(Post): #Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        super(PicturePost, self).__init__(text, timestamp)
+        self.image_url = image_url
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+        return '@{first_name} {last_name}: "{text}"\n\t{img}\n\t{date}'.format(
+            first_name=self.user.first_name,
+            last_name=self.user.last_name,
+            text=self.text,
+            img=self.image_url,
+            date=self.timestamp.strftime("%A, %b %d, %Y"))
+
+"""
+John Lennon: "Check my new guitar"
+  Pic URL: imgur.com/guitar.png
+  Friday, Feb 03, 2017
+"""
+
+class CheckInPost(Post): #Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super(CheckInPost, self).__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        return ('@{first_name} Checked In: "{text}"'
+                '\n\t{latitude}, {longitude}\n\t{date}').format(
+            first_name=self.user.first_name,
+            last_name=self.user.last_name,
+            text=self.text,
+            latitude= self.latitude,
+            longitude = self.longitude,
+            date=self.timestamp.strftime("%A, %b %d, %Y"))
+    
+"""
+John Checked In: "At Abbey Road Studios"
+  19.111, -9.2222
+  Friday, Feb 03, 2017
+"""
